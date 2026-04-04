ab0c41785537baebf6c24cd47e1f06dbe40ef0d5
MartinZeng <martin.zeng621@gmail.com>
2026-04-04 15:13:20 -0400

    allow external Microsoft accounts to sign in

--- job-tracker/src/auth/msalConfig.ts
+++ job-tracker/src/auth/msalConfig.ts
@@ -3,8 +3,7 @@ import type { Configuration, PopupRequest } from '@azure/msal-browser';
 export const msalConfig: Configuration = {
   auth: {
     clientId: '8c97609d-163b-416b-8f6d-7b4c4a9f4811',
-    authority:
-      'https://login.microsoftonline.com/ac8bfc1c-7627-4125-97ac-bed742934302',
+    authority: 'https://login.microsoftonline.com/common',
     redirectUri: import.meta.env.VITE_REDIRECT_URI ?? 'http://localhost:5173',
     postLogoutRedirectUri:
       import.meta.env.VITE_REDIRECT_URI ?? 'http://localhost:5173',
