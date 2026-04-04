4e811bffd461654d84bcd6918e92f161fdd8bc93
MartinZeng <martin.zeng621@gmail.com>
2026-04-04 11:03:01 -0400

    fix: disable native broker to remove network prompt

--- job-tracker/src/auth/msalConfig.ts
+++ job-tracker/src/auth/msalConfig.ts
@@ -5,13 +5,16 @@ export const msalConfig: Configuration = {
     clientId: '8c97609d-163b-416b-8f6d-7b4c4a9f4811',
     authority:
       'https://login.microsoftonline.com/ac8bfc1c-7627-4125-97ac-bed742934302',
-    redirectUri: 'http://localhost:5173',
-    postLogoutRedirectUri: 'http://localhost:5173',
+    redirectUri: import.meta.env.VITE_REDIRECT_URI ?? 'http://localhost:5173',
+    postLogoutRedirectUri:
+      import.meta.env.VITE_REDIRECT_URI ?? 'http://localhost:5173',
   },
   cache: {
     cacheLocation: 'sessionStorage',
   },
-  system: {},
+  system: {
+    allowNativeBroker: false,
+  },
 };
 
 export const loginRequest: PopupRequest = {
